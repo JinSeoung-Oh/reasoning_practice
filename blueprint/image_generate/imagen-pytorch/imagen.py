fac8002e3b58e3f00280863def02123bf26499f9
Jin-Seoung <jinseoungoh@gmail.com>
2023-07-25 23:11:54 +0900

    Update imagen.py

--- blueprint/image_generate/imagen-pytorch/imagen.py
+++ blueprint/image_generate/imagen-pytorch/imagen.py
@@ -41,9 +41,14 @@ images = torch.randn(4, 3, 256, 256).cuda()
 
 # feed images into imagen, training each unet in the cascade
 
-for i in (1, 2):
-    loss = imagen(images, texts = texts, unet_number = i)
-    loss.backward()
+loss = trainer(
+    images,
+    text_embeds = text_embeds,
+    unet_number = 1,            # training on unet number 1 in this example, but you will have to also save checkpoints and then reload and continue training on unet number 2
+    max_batch_size = 4          # auto divide the batch of 64 up into batch size of 4 and accumulate gradients, so it all fits in memory
+)
+
+trainer.update(unet_number = 1)
 
 
 images = imagen.sample(texts = [
