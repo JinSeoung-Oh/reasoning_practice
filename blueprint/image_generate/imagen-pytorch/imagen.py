fcfe9e54d0a47a1279baf6b7500ec42f37396ef1
Jin-Seoung <jinseoungoh@gmail.com>
2023-07-25 23:13:11 +0900

    Update imagen.py

--- blueprint/image_generate/imagen-pytorch/imagen.py
+++ blueprint/image_generate/imagen-pytorch/imagen.py
@@ -1,4 +1,4 @@
-## Train_code
+## Train_code with text
 import torch
 from imagen_pytorch import Unet, Imagen
 
@@ -20,7 +20,6 @@ unet2 = Unet(
     layer_cross_attns = (False, False, False, True)
 )
 
-# imagen, which contains the unets above (base unet and super resoluting ones)
 
 imagen = Imagen(
     unets = (unet1, unet2),
@@ -44,8 +43,8 @@ images = torch.randn(4, 3, 256, 256).cuda()
 loss = trainer(
     images,
     text_embeds = text_embeds,
-    unet_number = 1,            # training on unet number 1 in this example, but you will have to also save checkpoints and then reload and continue training on unet number 2
-    max_batch_size = 4          # auto divide the batch of 64 up into batch size of 4 and accumulate gradients, so it all fits in memory
+    unet_number = 1,            
+    max_batch_size = 4        
 )
 
 trainer.update(unet_number = 1)
@@ -58,3 +57,41 @@ images = imagen.sample(texts = [
 ], cond_scale = 3.)
 
 images.shape # (3, 3, 256, 256)
+
+
+
+## Train without text 
+
+unet1 = Unet(
+    dim = 32,
+    dim_mults = (1, 2, 4),
+    num_resnet_blocks = 3,
+    layer_attns = (False, True, True),
+    layer_cross_attns = False,
+    use_linear_attn = True
+)
+
+unet2 = SRUnet256(
+    dim = 32,
+    dim_mults = (1, 2, 4),
+    num_resnet_blocks = (2, 4, 8),
+    layer_attns = (False, False, True),
+    layer_cross_attns = False
+)
+
+
+imagen = Imagen(
+    condition_on_text = False,   # this must be set to False for unconditional Imagen
+    unets = (unet1, unet2),
+    image_sizes = (64, 128),
+    timesteps = 1000
+)
+
+trainer = ImagenTrainer(imagen).cuda()
+
+training_images = torch.randn(4, 3, 256, 256).cuda()
+
+loss = trainer(training_images, unet_number = 1)
+trainer.update(unet_number = 1)
+
+images = trainer.sample(batch_size = 16) # (16, 3, 128, 128)
