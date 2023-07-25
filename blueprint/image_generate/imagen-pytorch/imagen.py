ce8f3931a5116d9a31865ce295969eaf9e2dec2b
Jin-Seoung <jinseoungoh@gmail.com>
2023-07-25 23:11:22 +0900

    Update imagen.py

--- blueprint/image_generate/imagen-pytorch/imagen.py
+++ blueprint/image_generate/imagen-pytorch/imagen.py
@@ -1,1 +1,55 @@
+## Train_code
+import torch
+from imagen_pytorch import Unet, Imagen
 
+unet1 = Unet(
+    dim = 32,
+    cond_dim = 512,
+    dim_mults = (1, 2, 4, 8),
+    num_resnet_blocks = 3,
+    layer_attns = (False, True, True, True),
+    layer_cross_attns = (False, True, True, True)
+)
+
+unet2 = Unet(
+    dim = 32,
+    cond_dim = 512,
+    dim_mults = (1, 2, 4, 8),
+    num_resnet_blocks = (2, 4, 8, 8),
+    layer_attns = (False, False, False, True),
+    layer_cross_attns = (False, False, False, True)
+)
+
+# imagen, which contains the unets above (base unet and super resoluting ones)
+
+imagen = Imagen(
+    unets = (unet1, unet2),
+    image_sizes = (64, 256),
+    timesteps = 1000,
+    cond_drop_prob = 0.1
+).cuda()
+
+
+texts = [
+    'a child screaming at finding a worm within a half-eaten apple',
+    'lizard running across the desert on two feet',
+    'waking up to a psychedelic landscape',
+    'seashells sparkling in the shallow waters'
+]
+
+images = torch.randn(4, 3, 256, 256).cuda()
+
+# feed images into imagen, training each unet in the cascade
+
+for i in (1, 2):
+    loss = imagen(images, texts = texts, unet_number = i)
+    loss.backward()
+
+
+images = imagen.sample(texts = [
+    'a whale breaching from afar',
+    'young girl blowing out candles on her birthday cake',
+    'fireworks with blue and green sparkles'
+], cond_scale = 3.)
+
+images.shape # (3, 3, 256, 256)
